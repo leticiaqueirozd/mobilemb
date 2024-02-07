194fa3dc42e180e14a2eff93bf731deb0465a5de
leticiaqueirozd <leticiaqsouza@outlook.com>
2024-02-07 07:53:49 -0300

    perfil update

--- src/screens/PerfilScreen.js
+++ src/screens/PerfilScreen.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 const PerfilScreen = () => {
   return (
-    <View>
-      <Text>Página de Perfil</Text>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={require('../../assets/favicon.png')} style={styles.profileImage} />
+      </View>
+      <View style={styles.detailsContainer}>
+        <Text style={styles.name}>Leticia Queiroz</Text>
+        <Text style={styles.biography}>Biozinha simples só pra dizer que tem</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 50,
+    marginBottom: 30,
+  },
+  imageContainer: {
+    marginLeft: 20,
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  detailsContainer: {
+    marginLeft: 10,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  biography: {
+    fontSize: 16,
+    width: 175,
+  },
+});
 
 export default PerfilScreen;
